c4a722fc2e48c1d1736ce5dc81943d39841852c2
bndao <bndao@student.42.fr>
2020-05-23 01:29:43 +0200

    fix animation display

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,33 +8,41 @@ public class GameManager : MonoBehaviour
 	public PlayerMovement movement;
 	public RealJumpFeel jump;
 	public GameObject levelCompleteMessage;
+	private GameObject anim;
 
-	public float restartDelay = 2f;
+	public float restartLevelDelay = 2f;
+	public float nextLevelDelay = 10f;
 
 	private bool isGameOn = true;
 	void Awake()
 	{
 		this.FindNeededObjects();
+		anim.SetActive(false);
 		levelCompleteMessage.GetComponent<Image>().enabled = false;
 		levelCompleteMessage.GetComponent<Image>().transform.GetChild(0).gameObject.GetComponent<Text>().enabled = false;
 		levelCompleteMessage.GetComponent<Image>().transform.GetChild(1).gameObject.GetComponent<Text>().enabled = false;
 	}
 	public void LevelComplete() {
+		anim.SetActive(true);
+		anim.GetComponent<Animator>().enabled = true;
 		levelCompleteMessage.GetComponent<Image>().enabled = true;
 		levelCompleteMessage.GetComponent<Image>().transform.GetChild(0).gameObject.GetComponent<Text>().enabled = true;
 		levelCompleteMessage.GetComponent<Image>().transform.GetChild(1).gameObject.GetComponent<Text>().enabled = true;
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-	}
-	public void RestartLevel() {
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		Invoke("NextLevet", nextLevelDelay);
 	}
 	public void GameOver() {
 		if (isGameOn) {
 			movement.enabled = false;
 			jump.enabled = false;
 			isGameOn = false;
-			Invoke("RestartLevel", restartDelay);
+			Invoke("RestartLevel", restartLevelDelay);
 		}
+	}
+	public void RestartLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+	private void NextLevet() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 	private void FindNeededObjects() {
 		GameObject obj = GameObject.FindWithTag("Player") ?
@@ -45,5 +53,6 @@ public class GameManager : MonoBehaviour
 		movement = FindObjectOfType<PlayerMovement>();
 		jump = FindObjectOfType<RealJumpFeel>();
 		levelCompleteMessage = GameObject.FindWithTag("LevelWonDisplay");
+		anim = GameObject.Find("LevelComplete");
 	}
 }
